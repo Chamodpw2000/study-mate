5873655449a39c4435170d02e9349e6d754b94fd
Chamodpw2000 <143950123+Chamodpw2000@users.noreply.github.com>
2025-01-08 22:19:19 +0530

    Update AddNote.dart

--- lib/pages/AddNote.dart
+++ lib/pages/AddNote.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+//
 class Addnote extends StatefulWidget {
   const Addnote({super.key});
 
