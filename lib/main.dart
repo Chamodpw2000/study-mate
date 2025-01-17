3180a37614d1cdcc083ecd634ee7ee5c0cb514d0
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2025-01-17 19:07:46 +0530

    latest code

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:studymate/pages/MainFile.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
